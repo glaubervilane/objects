b387cb91755106d1e2f9e27d4ec33a117f80812a
Glauber Marques <glauberfmarques@gmail.com>
2023-05-11 19:17:36 -0600

    launch pair programming exercise with thereIsNoSpoon.js file

---
+++ thereIsNoSpoon.js
@@ -0,0 +1,47 @@
+//Done as part of pair programming with @KunleDev2
+
+const transpose = function (matrix) {
+  // Replace this code with your solution
+  let transposed = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let row = [];
+    for (let j = 0; j < matrix.lenght; j++) {
+      row.push(matrix[j][i]);
+    }
+    transposed.push(row);
+  }
+  return transposed;
+};
+
+// Do not edit this function.
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write("\n");
+  }
+};
+
+printMatrix(
+  transpose([
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+  ])
+);
+
+console.log("----");
+
+printMatrix(
+  transpose([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+);
+
+console.log("----");
+
+printMatrix(transpose([[1, 2, 3, 4, 5, 6, 7]]));
